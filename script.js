a6e44a89b83c98b3c9f33ac7b3a850a48f8dd80b
MuonNghiCode <minhquanpm1610@gmail.com>
2025-01-13 21:59:06 +0700

    done

---
+++ script.js
@@ -0,0 +1,304 @@
+const videos = [
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Rick Astley - Never Gonna Give You Up (Official Music Video)",
+    channel: "Rick Astley",
+    views: "1.6B views",
+    uploaded: "15 year ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+  {
+    thumbnail: "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    title: "Youtube",
+    channel: "Youtube",
+    views: "1M views",
+    uploaded: "2 days ago",
+  },
+];
+
+function createVideoCards() {
+  const videoSection = document.getElementById("video-section");
+
+  videos.forEach((video) => {
+    const videoCard = document.createElement("div");
+    videoCard.classList.add("video-card");
+
+    const thumbnail = document.createElement("img");
+    thumbnail.src = video.thumbnail;
+    thumbnail.alt = "Video Thumbnail";
+
+    const videoInfo = document.createElement("div");
+    videoInfo.classList.add("video-info");
+
+    const videoTitle = document.createElement("h4");
+    videoTitle.innerText = video.title;
+
+    const videoChannel = document.createElement("p");
+    videoChannel.innerText = video.channel;
+
+    const videoDetails = document.createElement("p");
+    videoDetails.innerText = `${video.views} • ${video.uploaded}`;
+
+    videoInfo.appendChild(videoTitle);
+    videoInfo.appendChild(videoChannel);
+    videoInfo.appendChild(videoDetails);
+    videoCard.appendChild(thumbnail);
+    videoCard.appendChild(videoInfo);
+
+    videoSection.appendChild(videoCard);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  createVideoCards();
+  const createButton = document.querySelector(".icon-create");
+  const dropdownMenu = createButton.querySelector(".dropdown-menu");
+
+  createButton.addEventListener("click", () => {
+    dropdownMenu.style.display =
+      dropdownMenu.style.display === "block" ? "none" : "block";
+  });
+
+  document.addEventListener("click", (event) => {
+    if (!createButton.contains(event.target)) {
+      dropdownMenu.style.display = "none";
+    }
+  });
+});
+
+document.querySelector(".menu-btn").addEventListener("click", function () {
+  document.querySelector("aside").classList.toggle("collapsed");
+  document.querySelector("main").classList.toggle("collapsed");
+  document.querySelector("footer").classList.toggle("collapsed");
+  document.querySelector(".categories-navbar").classList.toggle("collapsed");
+});
